21fda80ef1c1d6d7f4a36ceb043fad0a86a0c2b3
Corey Shaw <coreyrshaw@gmail.com>
2016-06-13 09:39:25 -0400

    Fixing TagRepository issues

--- src/main/java/tiy/lecture/organizer/TagRepository.java
+++ src/main/java/tiy/lecture/organizer/TagRepository.java
@@ -10,11 +10,6 @@ import java.util.List;
  */
 public interface TagRepository extends CrudRepository<Tag, Integer> {
 
-<<<<<<< HEAD
-  //  @Query("SELECT g FROM Note g WHERE g.getNotes Like ?1% ")
-List<Note>findByNote(String noteTag);
-=======
 //    @Query("SELECT g FROM Note g WHERE g.getNotes Like ?1% ")
 //List<Note>findByNote(String noteTag);
->>>>>>> 812298a1fb0677d461c10582b0afe25123013619
 }
