5da92e1a002571aa3ac4382c06125d0e66a53a55
godfatherfrancis <francis.aarons@gmail.com>
2016-06-23 12:17:27 -0400

    pre-load note content

--- src/main/java/tiy/lecture/organizer/LanguageRepository.java
+++ src/main/java/tiy/lecture/organizer/LanguageRepository.java
@@ -1,7 +1,12 @@
 package tiy.lecture.organizer;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface LanguageRepository extends CrudRepository<Language, Integer> {
 
+    @Query("SELECT g FROM Language g WHERE g.language LIKE ?1")
+    List<Language>findByLanguage(String language);
 }
